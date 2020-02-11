91492cbc1813d07326f901c38a1448552a4be5a6
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-02-11 23:26:43 +0100

    add method to implement the calculatePrice

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,40 @@
+# frozen_string_literal: true
+
+require 'rest-client'
+require 'json'
+
 module Types
   class QueryType < Types::BaseObject
+    graphql_name 'Calculate Price'
+    description 'The query fetches the current price of bitcoin in USD from `https://api.coindesk.com/v1/bpi/currentprice.json` and converts to Naira using provided transaction type and custom (USD/NGN) exchange rate'
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :calculate_price, PriceNairaType, null: false do
+      argument :type, TransactionType, required: true
+      argument :margin, Float, required: true
+      argument :exchange_rate, Float, required: true
+    end
+    def helpers
+      ActionController::Base.helpers
+    end
+
+    def calculate_price(type: nil, margin: nil, exchange_rate: nil)
+      response = RestClient.get 'https://api.coindesk.com/v1/bpi/currentprice.json'
+      bitcoin_price = JSON.parse(response)['bpi']['USD']['rate_float']
+      # puts helpers.number_to_currency(bitcoin_price, unit: '₦')
+      commission = (bitcoin_price * margin / 100)
+      final_price = 0
+      if type == 'buy'
+        final_price = (bitcoin_price + commission) * exchange_rate
+      elsif type == 'sell'
+        final_price = (bitcoin_price - commission) * exchange_rate
+      end
+      {
+        price: final_price,
+        formatted_price: helpers.number_to_currency(final_price, unit: '₦'),
+        time: JSON.parse(response)['time']['updated']
+      }
     end
   end
 end
